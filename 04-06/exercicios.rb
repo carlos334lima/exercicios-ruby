e8dcfa5cc77c3805e2e107668a31766e87c3abb0
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2020-09-24 14:48:41 -0300

    resolução dos exercicios da aula 04

--- 04-06/exercicios.rb
+++ 04-06/exercicios.rb
@@ -16,5 +16,25 @@ puts " Digite tres valores : "
 
 # ------------------------------------------------------------------------------------------------------------------------------------
 
-# Crie uma instryção que selciona o amior valor deste hash.
+# Crie uma instryção que selciona o amior valor deste hash. - 05
+
+
+V. LowLight
+4 meses atrás
+exercicio 2:
+
+
+count = 0 
+num = {}
+loop do 
+	print "Digite a chave e depois o valor: "
+	num[gets.chomp] = gets.chomp
+	count += 1
+	if count == 3
+		break
+	end
+end
+num.each do |key, value|
+	print "\nA chave é #{key} e o valor é #{value}"
+end
 
