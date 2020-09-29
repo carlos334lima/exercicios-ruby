287bcb211249304071eedbc5c3628a23683cadfc
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2020-09-29 10:40:54 -0300

    missão especial - ONEBITCODE

---
+++ compras/app.rb
@@ -0,0 +1,10 @@
+
+require "./produto.rb"
+require "./mercado.rb"
+
+meu_produto = Produto.new
+meu_produto.preco = 5
+meu_produto.nome = "beterraba"
+
+meu_mercado = Mercado.new(meu_produto) 
+meu_mercado.comprar
---
+++ compras/mercado.rb
@@ -0,0 +1,15 @@
+require "./produto.rb"
+
+class Mercado
+
+	def initialize (produto)
+		 @produto = produto
+	end
+
+	def comprar
+		puts "Voce comprou o produto #{@produto.nome} que custa #{@produto.preco}"
+	end
+
+end
+
+
